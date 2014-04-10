ec9f9f065350e931b87a028c8c276597b8bad5fb
Piotr M. Szczypinski <piotr.szczypinski@p.lodz.pl>
2014-04-10 22:46:08 +0200

    20140410b

--- treeskel.h
+++ treeskel.h
@@ -27,6 +27,12 @@
 #include <fstream>
 #include <math.h>
 
+/** \struct Coordinates3d
+ *  \brief Coordinates3d stores coordinates x, y, and z.
+ *
+ * This is simple coordinate vector.
+ * \ingroup Gap
+ */
 struct Coordinates3d
 {
     double x;
@@ -34,17 +40,39 @@ struct Coordinates3d
     double z;
 };
 
+/** \struct NodeIn3D
+ *  \brief NodeIn3D stores node description.
+ *
+ * This structure derives from the Coordinates3d.
+ * It stores coordinates of a node, number of other nodes
+ * connected to it and diameter of a vessel arround the node.
+ * \ingroup Gap
+ */
 struct NodeIn3D:Coordinates3d
 {
+/** Number of other nodes connected to this one. */
     unsigned int connections;
+/** Diameter of a vessel arround the node. */
     unsigned int diameter;
 };
 
+/** \struct BasicBranch
+ *  \brief BasicBranch stores indexes of nodes building a branch.
+ * BasicBranch stores indexes of nodes building a branch.
+ * \ingroup Gap
+ */
 struct BasicBranch
 {
     std::vector<unsigned int> nodeIndex;
 };
 
+/** \struct TreeSkeletonStructure
+ *  \brief TreeSkeletonStructure stores information on a vessel tree.
+ * TreeSkeletonStructure stores indexes of nodes building individual
+ * branches in branches vector and corresponding nodes description in
+ * nodes vector.
+ * \ingroup Gap
+ */
 struct TreeSkeletonStructure
 {
     std::vector <NodeIn3D> nodes;
@@ -52,26 +80,49 @@ struct TreeSkeletonStructure
 };
 
 //-------------------------------------------------------------------------------------
+/** \classt TreeSkeleton
+ *  \brief TreeSkeleton derives from TreeSkeletonStructure.
+ * TreeSkeleton defines functions to safely add and remove branches
+ * in TreeSkeleton structure.
+ * \ingroup Gap
+ */
 class TreeSkeleton:TreeSkeletonStructure
 {
 public:
+/** Constructor. The mindistance parameter defines a minimum distance
+ * below which tip nodes of branch being added is joined with the closest
+ * node of a tree.*/
     TreeSkeleton(double mindistance = 0.5);
+
+/** Saves a tree to a file of a given fileName in interna format
+ * format=0 or as separate branches format=1.*/
     bool saveTree(char *fileName, unsigned int format);
+/** Loads a tree from a file of a given fileName.*/
     bool loadTree(const char *fileName);
 
+/** Returns count of branches.*/
     unsigned int count(void);
+/** Returns count of nodes in a branch with an index branchIndex.*/
     int count(unsigned int branchIndex);
+/** Returns count of all the nodes forming the tree.*/
     unsigned int nodeCount(void);
 
+/** Returns vector of nodes formin a branch with an index branchIndex.*/
     std::vector<NodeIn3D> branch(unsigned int branchIndex);
+/** Returns a subsequent Inode node of a branch with an index branchI.*/
     NodeIn3D node(unsigned int branchI, unsigned int nodeI);
+/** Returns a subsequent node of a tree.*/
     NodeIn3D node(unsigned int index);
+/** Exchanges a node given by index with a new information provided
+ *  in a newNode.*/
     bool setNode(NodeIn3D newNode, unsigned int index);
+/** Adds a new branch newBranch to the tree.*/
     bool addBranch(std::vector<NodeIn3D> newBranch);
+/** Removes a branch of an index index from the tree.*/
     bool removeBranch(unsigned int index);
 
 private:
-    double joinDistance;
+    double joinDistance; /**< Distance to join nodes, set by the constructor.*/
 };
 
 #endif // TREESKEL_H
